f9854e4f53e65b7aa8cc2f99887d403ba357a286
ma2.6chicago <mollygirl1232@gmail.com>
2025-07-11 09:29:30 -0500

    Initial Commit

---
+++ birthdaysapp/birthdaysappApp.swift
@@ -0,0 +1,17 @@
+//
+//  birthdaysappApp.swift
+//  birthdaysapp
+//
+//  Created by Scholar on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct birthdaysappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
