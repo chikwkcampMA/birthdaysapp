6acdaa2e71767e8068b9da568d515fc524fe4447
ma2.6chicago <mollygirl1232@gmail.com>
2025-07-11 10:31:47 -0500

    we created a birthday app

--- birthdaysapp/ContentView.swift
+++ birthdaysapp/ContentView.swift
@@ -8,17 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = [
+        Friend(name: "John", birthday: .now),
+        Friend(name: "Samantha", birthday:Date(timeIntervalSince1970: 0)) ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(friends, id: \.name) { friend in
+                HStack {
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+            }
+            .navigationTitle("awesome bday app")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                        //textFieldStyle adds a box around the text input section
+                    }
+                    Button("pls save me") {
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        newName = ""
+                        newBirthday = .now
+                    }
+                    .padding()
+                    .background(.bar)
+                }
+            }
         }
-        .padding()
     }
 }
+    
+    #Preview {
+        ContentView()
+    }
 
-#Preview {
-    ContentView()
-}
